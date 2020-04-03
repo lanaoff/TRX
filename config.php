fc1328f7b964ff4bc9a617b48103382c99d4f261
lanaoff <61947897+lanaoff@users.noreply.github.com>
2020-04-04 00:59:59 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?
+/* user agent terserah mau gati atao gak */
+$user_agent = "Mozilla/5.0 (Linux; Android 9; RMX1911) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile Safari/537.36";
+$PHPSESSID = "7cd95204903cd9c2cd190b5c38e49cc1";
+$TRXToken = "eTexhW4YNBbPOcCl4l74zAaAJM4eivaJ";
+
+
